7f8100a9b05d04d6213674e18b8658be107bbcb8
Nero Sule <sulenero@gmail.com>
2016-11-30 18:03:36 +0000

    Initial

---
+++ src/Tempest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tempest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class Generator
+    {
+        
+    }
+
+    /// <summary>
+    /// Transforms a file.
+    /// Could be:
+    /// TokenTransformer
+    /// XdtTransformer
+    /// JsonTransformer
+    /// 
+    /// </summary>
+    public class Transformer
+    {
+        
+    }
+
+    
+
+    // What does a template generator need?
+
+    // Copy files from one location to another, possibly rename
+    // Replace text (tokens) in files
+    // Modify project structure (edit project files) - serialization?
+    // Simple syntax
+
+
+    
+}
