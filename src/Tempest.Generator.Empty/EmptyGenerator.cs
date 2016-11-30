6b8a2fb3e6e7c0df5dbb6e4c9c765cc8dfde1854
Nero Sule <sulenero@gmail.com>
2016-11-30 18:57:21 +0000

    Maybe got the flow?

---
+++ src/Tempest.Generator.Empty/EmptyGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tempest.Generator.Empty
+{
+    public class EmptyGenerator
+    {
+        public IEnumerable<string> FilesToHaveAtTheEnd { get; set; } = new[]
+        {
+            "project.json",
+            "EmptyGenerator.cs",
+            "Tempest.Generator.Empty.xproj"
+        };
+    }
+}
--- src/Tempest/Program.cs
+++ src/Tempest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,25 +14,98 @@ namespace Tempest
     }
 
 
-    public class Generator
-    {
-        
-    }
+    // 1) Generator generates (out of thin air or by copying or by downloading or whatever) a stream
+    // 2) Transformer takes the stream and does whatever (like changing all occurrences of 'Microsoft' into 'YourMom') and passes that on as new stream
+    // 3) Emitter takes the stream and emits it wherever (like into a file, or maybe into github repo, trololo)
+    // 4) Profit, because no steps are "???" =D
+
 
     /// <summary>
-    /// Transforms a file.
+    /// Generates an output stream
+    /// Could be:
+    /// GenerateEmpty
+    /// GenerateByCopying
+    /// GenerateFromWebstream
+    /// </summary>
+    public abstract class Generator
+    {
+        public virtual GenerationResult Generate(GenesisContext context)
+        {
+            return new GenerationResult() {OutputStream = GenerateCore(context)};
+        }
+
+        protected abstract Stream GenerateCore(GenesisContext context);
+    }
+
+
+    /// <summary>
+    /// Transforms the input stream into output transformation result
     /// Could be:
     /// TokenTransformer
     /// XdtTransformer
     /// JsonTransformer
     /// 
     /// </summary>
-    public class Transformer
+    public abstract class Transformer
+    {
+        public virtual TransformationResult Transform(TransformerContext context)
+        {
+            return new TransformationResult()
+            {
+                OutputStream = TransformCore(context)
+            };
+        }
+
+        protected virtual Stream TransformCore(TransformerContext context)
+        {
+            return context.InputStream;
+        }
+    }
+
+    public class Emitter
+    {
+        public virtual EmissionResult Emit(EmissionContext context)
+        {
+            return new EmissionResult();
+        }
+    }
+
+    public class GenerationResult
+    {
+        public Stream OutputStream { get; set; }
+    }
+
+
+    public class TransformationResult
+    {
+        public Stream OutputStream { get; set; }
+    }
+
+    public class EmissionResult
     {
         
     }
 
-    
+    public class TransformerContext
+    {
+        public Stream InputStream { get; set; }
+
+        public string ReadInputAsString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GenesisContext
+    {
+        
+    }
+
+    public class EmissionContext
+    {
+        
+    }
+}
 
     // What does a template generator need?
 
