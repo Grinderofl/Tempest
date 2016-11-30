82d5978ccd19d38109dff36c91b97a68ed221860
Nero Sule <sulenero@gmail.com>
2016-11-30 19:47:49 +0000

    More test

---
+++ test/Tempest.CoreTests/Transformation/TransformersTests.cs
@@ -0,0 +1,34 @@
+﻿using Tempest.Transformation;
+using Tempest.Utils;
+using Xunit;
+
+namespace Tempest.CoreTests.Transformation
+{
+    public class TransformersTests
+    {
+        public class EmptyTransformerTests
+        {
+            [Fact]
+            public void transforms_to_empty()
+            {
+                var transformer = Transformers.Empty;
+                var result = transformer.Transform(new TransformerContext() {InputStream = "TestContent".ToStream()});
+                var resultString = result.OutputStream.ReadAsString();
+                Assert.Equal("", resultString);
+
+            }
+        }
+
+        public class NoOpTransformerTests
+        {
+            [Fact]
+            public void does_not_transform()
+            {
+                var transformer = Transformers.NoOp;
+                var result = transformer.Transform(new TransformerContext() {InputStream = "TestContent".ToStream()});
+                var resultString = result.OutputStream.ReadAsString();
+                Assert.Equal("TestContent", resultString);
+            }
+        }
+    }
+}
