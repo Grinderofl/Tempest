1243f664f6564e6d25229204160b89f8d2b70d07
Nero Sule <sulenero@gmail.com>
2016-12-04 21:03:46 +0000

    Updates included package types

--- install.ps1
+++ install.ps1
@@ -7,6 +7,11 @@ if(!$PSScriptRoot){
     $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 }
 
+if (!(Test-Path $TEMPEST_DIR)) {
+    Write "Creating tempest directory..."
+    New-Item -Path $TEMPEST_DIR -Type directory | out-null
+}
+
 if (!(Test-Path $PACKAGES_FILE)) {
     Write "Downloading packages.config..."
     try { (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/Grinderofl/Tempest/develop/install/packages.config", $PACKAGES_FILE) } catch {
@@ -15,27 +20,29 @@ if (!(Test-Path $PACKAGES_FILE)) {
 }
 
 
-# if (!(Test-Path $NUGET_EXE)) {
-    # Write "Trying to find nuget.exe in PATH..."
-    # $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_) }
-    # $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
-    # if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
-        # Write-Verbose -Message "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
-        # $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
-    # }
-# }
+if (!(Test-Path $NUGET_EXE)) {
+    Write "Trying to find nuget.exe in PATH..."
+	Write $Env:Path
+    $existingPaths = $Env:Path -Split ';' | Where-Object { (![string]::IsNullOrEmpty($_)) -and (Test-Path $_) }
+    $NUGET_EXE_IN_PATH = Get-ChildItem -Path $existingPaths -Filter "nuget.exe" | Select -First 1
+    if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH.FullName)) {
+        Write "Found in PATH at $($NUGET_EXE_IN_PATH.FullName)."
+        $NUGET_EXE = $NUGET_EXE_IN_PATH.FullName
+    }
+}
 
 if (!(Test-Path $NUGET_EXE)) {
     Write "Downloading NuGet.exe..."
     try {
-        (New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
+        #(New-Object System.Net.WebClient).DownloadFile($NUGET_URL, $NUGET_EXE)
     } catch {
         Throw "Could not download NuGet.exe."
     }
 }
 
 $ENV:NUGET_EXE = $NUGET_EXE
-
+Push-Location
+Set-Location $TEMPEST_DIR
 Write "Restoring from NuGet..."
 #$NuGetOutput = Invoke-Expression "&`"./$NUGET_EXE`" install -ExcludeVersion"
 $NuGetOutput = Invoke-Expression "&`"./$NUGET_EXE`" install Tempest -Pre -ExcludeVersion"
@@ -46,9 +53,9 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 Write ($NuGetOutput | out-string)
-$currentPath = $MyInvocation.MyCommand.Path
+$currentPath = (Get-Item -Path ".\" -Verbose).FullName
 Write $currentPath
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $currentPath, [EnvironmentVariableTarget]::User)
+#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $currentPath, [EnvironmentVariableTarget]::User)
 
 # $oldpath = (Get-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PATH).path
 # $currentPath = $MyInvocation.MyCommand.Path
@@ -56,3 +63,4 @@ Write $currentPath
 # Set-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PATH –Value $newPath
 
 $env:Path += ";$currentPath"
+Pop-Location
